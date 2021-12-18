3f58fec631790c2a57f6afd4f9590bebcf12a590
Andrei Kozel <kozel.andrei.94@gmail.com>
2021-12-18 11:17:06 +0100

    isWeb3Loaded

--- components/ui/common/navbar/index.js
+++ components/ui/common/navbar/index.js
@@ -2,7 +2,7 @@ import { useWeb3 } from "@components/providers";
 import Link from "next/link";
 
 export default function Navbar({}) {
-  const { connect } = useWeb3();
+  const { connect, isWeb3Loaded } = useWeb3();
 
   return (
     <section>
@@ -32,12 +32,21 @@ export default function Navbar({}) {
                   Wishlist
                 </a>
               </Link>
-              <span
-                onClick={connect}
-                className="px-8 py-3 border rounded-md text-base font-medium text-white bg-indigo-600 hover:bg-indigo-700 cursor-pointer"
-              >
-                Connect
-              </span>
+              {isWeb3Loaded ? (
+                <span
+                  onClick={connect}
+                  className="px-8 py-3 border rounded-md text-base font-medium text-white bg-indigo-600 hover:bg-indigo-700 cursor-pointer"
+                >
+                  Connect
+                </span>
+              ) : (
+                <span
+                  onClick={connect}
+                  className="px-8 py-3 border rounded-md text-base font-medium text-white bg-indigo-600 hover:bg-indigo-700 cursor-pointer"
+                >
+                  Install Metamask
+                </span>
+              )}
             </div>
           </div>
         </nav>
