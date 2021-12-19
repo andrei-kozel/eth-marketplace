d47947afcadf1e7974877180d0f1c8cf67524677
Andrei Kozel <kozel.andrei.94@gmail.com>
2021-12-19 12:39:31 +0100

    refactor

--- components/providers/web3/index.js
+++ components/providers/web3/index.js
@@ -40,14 +40,15 @@ export default function Web3Provider({ children }) {
   }, []);
 
   const _web3Api = useMemo(() => {
+    const { web3, provider } = web3Api;
     return {
       ...web3Api,
-      isWeb3Loaded: web3Api.web3 != null,
-      hooks: setupHooks(web3Api.web3),
-      connect: web3Api.provider
+      isWeb3Loaded: web3 != null,
+      getHooks: () => setupHooks(web3),
+      connect: provider
         ? async () => {
             try {
-              await web3Api.provider.request({ method: "eth_requestAccounts" });
+              await provider.request({ method: "eth_requestAccounts" });
             } catch {
               location.reload();
             }
@@ -66,4 +67,10 @@ export default function Web3Provider({ children }) {
 
 export function useWeb3() {
   return useContext(Web3Context);
+}
+
+export function useHooks(resolve) {
+  const { getHooks } = useWeb3();
+  const hooks = getHooks();
+  return resolve(hooks);
 }
