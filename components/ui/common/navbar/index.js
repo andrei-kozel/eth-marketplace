d96e61809ebb39306ceb9e0262e487221cb98887
Andrei Kozel <kozel.andrei.94@gmail.com>
2021-12-19 12:09:33 +0100

    useAccount hook

--- components/ui/common/navbar/index.js
+++ components/ui/common/navbar/index.js
@@ -1,9 +1,10 @@
-import { useWeb3 } from "@components/providers";
 import Link from "next/link";
+import { useWeb3 } from "@components/providers";
 import { Button } from "@components/ui/common";
 
 export default function Navbar() {
-  const { connect, isLoading, isWeb3Loaded } = useWeb3();
+  const { connect, isLoading, isWeb3Loaded, hooks } = useWeb3();
+  const { account } = hooks.useAccount();
 
   return (
     <section>
